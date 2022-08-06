c88b78f28d118baee8c298d037783047e2c2a35b
HursitTarcan <hursittarcan@gmail.com>
2022-08-07 00:16:23 +0200

    read input.txt done

--- day 1/app.js
+++ day 1/app.js
@@ -1,4 +1,13 @@
-let  fs = require('fs');
+const {readFileSync, promises: fsPromises} = require('fs');
 
-fs.readFile('foo.json', bar)
+function syncReadFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
 
+    const arr = contents.split(/\r?\n/);
+
+    console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+    return arr;
+}
+
+syncReadFile('./input.txt');
