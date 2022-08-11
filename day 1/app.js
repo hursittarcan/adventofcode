b8f89d2031503e8e315aa8798d4bfe06e15c72a1
HursitTarcan <hursittarcan@gmail.com>
2022-08-11 20:22:41 +0200

    part 1 + 2 completed

--- day 1/app.js
+++ day 1/app.js
@@ -1,3 +1,5 @@
+//DAY 1 - PART 1
+
 const { readFileSync } = require('fs');
 
 function readFile(filename) {
@@ -8,12 +10,27 @@ function readFile(filename) {
 let measurements = readFile('input.txt');
 
 let totalMeasurementIncreases = 0;
-for (let i = 0; i < measurements.length; i++) {
-    let previousNumber = measurements[i - 1];
-    let currentNumber = measurements[i];
+for (let i = 1; i < measurements.length; i++) {
+    let previousNumber = parseInt(measurements[i - 1]);
+    let currentNumber = parseInt(measurements[i]);
 
     if (currentNumber > previousNumber) {
         totalMeasurementIncreases++;
     }
 }
-console.log(totalMeasurementIncreases);
+
+console.log("PART 1: " + totalMeasurementIncreases);
+
+//DAY 1 - PART 2
+
+let accurateMeasurementIncreases = 0;
+for (let i = 3; i < measurements.length; i++) {
+    let previousNumber = parseInt(measurements[i - 1])  + parseInt(measurements[i - 2]) + parseInt(measurements[i - 3]);
+    let currentNumber = parseInt(measurements[i]) + parseInt(measurements[i - 1]) + parseInt(measurements[i - 2]);
+
+    if (currentNumber > previousNumber) {
+        accurateMeasurementIncreases++;
+    }
+}
+
+console.log("PART 2: " + accurateMeasurementIncreases);
