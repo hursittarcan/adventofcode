328870addd13ad255db4d762efadefc3bc798dd3
HursitTarcan <hursittarcan@gmail.com>
2022-08-11 20:57:36 +0200

    day two refactored

--- day 2/app.js
+++ day 2/app.js
@@ -7,43 +7,30 @@ function readFile(filename) {
 
 let roadmap = readFile('input.txt');
 
-let depth = 0;
-let position = 0;
-
-for (let i = 0; i < roadmap.length; i++) {
-    let step = roadmap[i].split(" ");
-
-    if (step[0] === "forward") {
-        position += parseInt(step[1]);
-    }
-
-    if (step[0] === "up") {
-        depth -= parseInt(step[1]);
-    }
-    else if (step[0] === "down") {
-        depth += parseInt(step[1]);
-    }
-}
-
 let aim = 0;
+let depth = 0;
 let depthWithAim = 0;
+let position = 0;
 let positionWithAim = 0;
 
 for (let i = 0; i < roadmap.length; i++) {
     let step = roadmap[i].split(" ");
 
     if (step[0] === "forward") {
+        position += parseInt(step[1]);
         positionWithAim += parseInt(step[1]);
         depthWithAim += parseInt(step[1]) * aim;
     }
 
     if (step[0] === "up") {
+        depth -= parseInt(step[1]);
         aim -= parseInt(step[1]);
     }
     else if (step[0] === "down") {
+        depth += parseInt(step[1]);
         aim += parseInt(step[1]);
     }
 }
 
-console.log("PART 2: " + positionWithAim * depthWithAim);
 console.log("PART 1: " + position * depth);
+console.log("PART 2: " + positionWithAim * depthWithAim);
