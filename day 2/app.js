6386ff22f40f765e379724c57807262f76be6ae3
HursitTarcan <hursittarcan@gmail.com>
2022-08-11 20:39:17 +0200

    day two - part one completed

---
+++ day 2/app.js
@@ -0,0 +1,30 @@
+const { readFileSync } = require('fs');
+
+function readFile(filename) {
+    const contents = readFileSync(filename, 'utf-8');
+    return contents.split(/\r?\n/);
+}
+
+let roadmap = readFile('input.txt');
+
+let depth = 0;
+let position = 0;
+
+for (let i = 0; i < roadmap.length; i++) {
+    let step = roadmap[i].split(" ");
+
+    if (step[0] === "forward") {
+        position += parseInt(step[1]);
+    }
+
+    if (step[0] === "up") {
+        depth -= parseInt(step[1]);
+    }
+    else if (step[0] === "down") {
+        depth += parseInt(step[1]);
+    }
+}
+
+console.log(position) ;
+console.log(depth);
+console.log(position * depth);
