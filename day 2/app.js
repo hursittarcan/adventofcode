0e24b27fc59ab14c298cc6979764c6b442e5cf58
HursitTarcan <hursittarcan@gmail.com>
2022-08-11 20:52:51 +0200

    part two first edition (wrong)

--- day 2/app.js
+++ day 2/app.js
@@ -26,3 +26,30 @@ for (let i = 0; i < roadmap.length; i++) {
 }
 
 console.log("PART 1: " + position * depth);
+
+let aim = 0;
+let depthWithAim = 0;
+let positionWithAim = 0;
+
+for (let i = 0; i < roadmap.length; i++) {
+    let step = roadmap[i].split(" ");
+
+    if (step[0] === "forward") {
+        if (aim > 0) {
+            positionWithAim += parseInt(step[1]);
+        } else {
+            positionWithAim += parseInt(step[1]);
+        }
+    }
+
+    if (step[0] === "up") {
+        depthWithAim -= parseInt(step[1]);
+        aim -= parseInt(step[1]);
+    }
+    else if (step[0] === "down") {
+        depthWithAim += parseInt(step[1]);
+        aim += parseInt(step[1]);
+    }
+}
+
+console.log("PART 1: " + positionWithAim * depthWithAim);
