fcdca1d26c41896b358e54888e62f8fe1ca68744
HursitTarcan <hursittarcan@gmail.com>
2022-08-11 20:27:19 +0200

    removed comments

--- day 1/app.js
+++ day 1/app.js
@@ -1,8 +1,3 @@
-//2021 - DAY ONE (PART ONE + PART TWO)
-
-//This code reads input.txt and puts everything in an array.
-//NOTE, the array is of type string, that's why I use parseInt in this exercise.
-//------------------------------------------------------------
 const { readFileSync } = require('fs');
 
 function readFile(filename) {
@@ -11,10 +6,7 @@ function readFile(filename) {
 }
 
 let measurements = readFile('input.txt');
-//------------------------------------------------------------
 
-//This is the code for part one of the exercise.
-//------------------------------------------------------------
 let totalMeasurementIncreases = 0;
 for (let i = 1; i < measurements.length; i++) {
     let previousNumber = parseInt(measurements[i - 1]);
@@ -25,11 +17,6 @@ for (let i = 1; i < measurements.length; i++) {
     }
 }
 
-console.log("PART 1: " + totalMeasurementIncreases);
-//------------------------------------------------------------
-
-//This is the code for part two of the exercise.
-//------------------------------------------------------------
 let accurateMeasurementIncreases = 0;
 for (let i = 3; i < measurements.length; i++) {
     let previousNumber = parseInt(measurements[i - 1])  + parseInt(measurements[i - 2]) + parseInt(measurements[i - 3]);
@@ -40,5 +27,5 @@ for (let i = 3; i < measurements.length; i++) {
     }
 }
 
+console.log("PART 1: " + totalMeasurementIncreases);
 console.log("PART 2: " + accurateMeasurementIncreases);
-//------------------------------------------------------------
